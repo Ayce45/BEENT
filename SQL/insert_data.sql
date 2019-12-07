256fa5cdd44859113a706833733de516fb4b6d43
Ayce <32338891+Ayce45@users.noreply.github.com>
2019-12-07 20:18:46 +0100

    Update insert_data.sql

--- SQL/insert_data.sql
+++ SQL/insert_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le :  ven. 06 déc. 2019 à 13:16
+-- Généré le :  sam. 07 déc. 2019 à 20:17
 -- Version du serveur :  10.4.8-MariaDB
 -- Version de PHP :  7.1.33
 
@@ -31,27 +31,18 @@ INSERT INTO `Annee` (`id`, `code`) VALUES
 (2, '2020');
 
 --
--- Déchargement des données de la table `Type`
+-- Déchargement des données de la table `Classe`
 --
 
-INSERT INTO `Type` (`id`, `libelle`) VALUES
-(1, 'CM'),
-(2, 'TD'),
-(3, 'TP'),
-(4, 'CC'),
-(5, 'CT');
+INSERT INTO `Classe` (`id`, `code`, `id_Filiere`) VALUES
+(1, 1, 1);
 
 --
--- Déchargement des données de la table `Salle`
+-- Déchargement des données de la table `Cours`
 --
 
-INSERT INTO `Salle` (`id`, `code`, `capacite`, `pc`) VALUES
-(1, 'E001', 20, 1),
-(2, 'E002', 30, 0),
-(3, 'E003', 30, 0),
-(4, 'E004', 30, 0),
-(5, 'E005', 30, 0),
-(6, 'AMPHI HERBRAND', 100, 0);
+INSERT INTO `Cours` (`id`, `id_Enseignant`, `id_Salle`, `id_Matiere`, `id_Groupe`, `id_Type`, `debut`, `fin`) VALUES
+(1, 1, 1, 1, 1, 1, '2019-12-09 08:00:00', '2019-12-09 10:00:00');
 
 --
 -- Déchargement des données de la table `Enseignant`
@@ -62,18 +53,18 @@ INSERT INTO `Enseignant` (`id`, `nom`, `prenom`, `rue`, `cp`, `ville`, `tel`, `e
 (2, 'JUGE', 'Evan', '510 rue neuve', '45600', 'Olivet', '0777323234', 'evan.juge@univ-oreans.fr');
 
 --
+-- Déchargement des données de la table `Enseignant_Matiere`
+--
+
+INSERT INTO `Enseignant_Matiere` (`id`, `id_Enseignant`) VALUES
+(1, 2);
+
+--
 -- Déchargement des données de la table `Filiere`
 --
 
 INSERT INTO `Filiere` (`id`, `libelle`) VALUES
 (1, 'L3 MIAGE');
-
---
--- Déchargement des données de la table `Classe`
---
-
-INSERT INTO `Classe` (`id`, `code`, `id_Filiere`) VALUES
-(1, 1, 1);
 
 --
 -- Déchargement des données de la table `Groupe`
@@ -92,21 +83,6 @@ INSERT INTO `Matiere` (`id`, `libelle`) VALUES
 (2, 'Réseaux');
 
 --
--- Déchargement des données de la table `Enseignant_Matiere`
---
-
-INSERT INTO `Enseignant_Matiere` (`id`, `id_Enseignant`) VALUES
-(1, 2);
-
---
--- Déchargement des données de la table `Semestre`
---
-
-INSERT INTO `Semestre` (`id`, `code`, `id_Annee`) VALUES
-(1, 5, 1),
-(2, 6, 2);
-
---
 -- Déchargement des données de la table `Matiere_Semestre`
 --
 
@@ -119,11 +95,35 @@ INSERT INTO `Matiere_Semestre` (`id`, `id_Semestre`, `id_Filiere`, `id_Type`, `h
 (2, 1, 1, 3, 24);
 
 --
--- Déchargement des données de la table `Cours`
+-- Déchargement des données de la table `Salle`
 --
 
-INSERT INTO `Cours` (`id`, `id_Salle`, `id_Matiere`, `id_Groupe`, `debut`, `fin`) VALUES
-(1, 6, 1, 1, '2019-12-09 08:00:00', '2019-12-09 10:00:00');
+INSERT INTO `Salle` (`id`, `code`, `capacite`, `pc`) VALUES
+(1, 'E001', 20, 1),
+(2, 'E002', 30, 0),
+(3, 'E003', 30, 0),
+(4, 'E004', 30, 0),
+(5, 'E005', 30, 0),
+(6, 'AMPHI HERBRAND', 100, 0);
+
+--
+-- Déchargement des données de la table `Semestre`
+--
+
+INSERT INTO `Semestre` (`id`, `code`, `id_Annee`) VALUES
+(1, 5, 1),
+(2, 6, 2);
+
+--
+-- Déchargement des données de la table `Type`
+--
+
+INSERT INTO `Type` (`id`, `libelle`) VALUES
+(1, 'CM'),
+(2, 'TD'),
+(3, 'TP'),
+(4, 'CC'),
+(5, 'CT');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
